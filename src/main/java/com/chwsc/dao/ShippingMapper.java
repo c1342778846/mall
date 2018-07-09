3e12baacc183ec520834b6571c62742d9fc93d1f
chw <1342778846@qq.com>
2018-07-09 23:17:59 +0800

    demo2

---
+++ src/main/java/com/chwsc/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.chwsc.dao;
+
+import com.chwsc.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
